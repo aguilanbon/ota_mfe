2a271d70b383db79dcbd3fa8a3add252d22b4fcf
Bon Aguilan <aguilanbon@gmail.com>
2024-10-17 10:14:51 +0800

    init navbar buttons component and items

--- src/components/navigation_bar.jsx
+++ src/components/navigation_bar.jsx
@@ -1,9 +1,16 @@
 import NavigationButton from "./navigation_buttons";
+import { RiCoupon2Line } from "react-icons/ri";
+import { GoHome, GoPerson } from "react-icons/go";
 
 function NavigationBar() {
   return (
-    <div className="h-[60px] bg-white flex w-full justify-between px-10">
-      <NavigationButton label={"wow"} />
+    <div className="h-[60px] bg-white flex w-full justify-around text-primary">
+      <NavigationButton icon={<GoHome height={24} />} label={"Home"} />
+      <NavigationButton
+        icon={<RiCoupon2Line height={24} />}
+        label={"My Coupons"}
+      />
+      <NavigationButton icon={<GoPerson height={24} />} label={"Me"} />
     </div>
   );
 }
