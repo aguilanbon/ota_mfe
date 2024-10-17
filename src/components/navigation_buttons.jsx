fe11abbbbeb0820dbf97a2eadb574aea250e294b
Bon Aguilan <aguilanbon@gmail.com>
2024-10-17 11:07:03 +0800

    fix paddings and size nav buttons

--- src/components/navigation_buttons.jsx
+++ src/components/navigation_buttons.jsx
@@ -1,12 +1,12 @@
 function NavigationButton({ icon, label, isActive, onClick }) {
   return (
     <div
-      className={`flex flex-col items-center cursor-pointer transition-colors ${
+      className={`flex flex-col items-center cursor-pointer transition-colors justify-center ${
         isActive ? "hover:text-primary" : "hover:text-gray-300"
       }`}
       onClick={onClick}
     >
-      <button className="p-2">{icon}</button>
+      <button className="">{icon}</button>
       <span className="text-xs">{label}</span>
     </div>
   );
