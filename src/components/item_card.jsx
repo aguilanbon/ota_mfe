4218df8597e43ac6dec2a7cd0d1ab7e60be809f2
Bon Aguilan <aguilanbon@gmail.com>
2024-10-17 13:37:15 +0800

    init custom card

---
+++ src/components/item_card.jsx
@@ -0,0 +1,14 @@
+function ItemCard() {
+  return (
+    <div className="flex-shrink-0 w-[183px] h-[220px] rounded-xl shadow-md  bg-white">
+      <div className="h-[120px] bg-gray-100 rounded-t-lg" />
+      <div className="flex flex-col p-2">
+        <h3 className="font-medium">Item Title</h3>
+        <p className="text-sm text-gray-600">Item description</p>
+        <div className="mt-2 text-blue-600 font-bold">$99.99</div>
+      </div>
+    </div>
+  );
+}
+
+export default ItemCard;
