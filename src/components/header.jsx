dd541a1f9f9ae846a2cab18c8f061832b687ad3a
Bon Aguilan <aguilanbon@gmail.com>
2024-10-17 23:20:49 +0800

    header profile image use mock image from picsum, added fallback to letter of first name

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 function Header() {
+  const [profileImageError, setProfileImageError] = useState(false);
+
   return (
     <div className="flex flex-col justify-center h-[94px] text-white">
       <div className="h-[46px] flex flex-row justify-start items-center">
         <div className="w-12 h-12 bg-orange-200 mx-4 rounded-full flex items-center justify-center">
-          <p>N</p>
+          {!profileImageError ? (
+            <img
+              src="https://picsum.photos/200/300"
+              className="w-full h-full object-cover rounded-full"
+              onError={() => setProfileImageError(true)}
+            />
+          ) : (
+            <div>N</div>
+          )}
         </div>
         <div className="flex flex-col">
           <p className="text-sm">Welcome to e-commerce</p>
