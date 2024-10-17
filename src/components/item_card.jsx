093ec60f130141aea6b00e8b81e825c12a765311
Bon Aguilan <aguilanbon@gmail.com>
2024-10-17 23:03:45 +0800

    set fallback icon for image error, catch image onerror

--- src/components/item_card.jsx
+++ src/components/item_card.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { PiImageBroken } from "react-icons/pi";
+
 function ItemCard({ product }) {
   const {
     product_name,
@@ -8,29 +11,40 @@ function ItemCard({ product }) {
     progress = 0,
   } = product;
 
+  const [productImageError, setProductImageError] = useState(false);
+  const [brandImageError, setBrandImageError] = useState(false);
+
   return (
     <div className="w-[183px] h-[250px] md:h-[272px] md:w-[220px] xl:h-[310px] xl:w-[248px]   rounded-xl shadow-md bg-white flex flex-col overflow-hidden cursor-pointer hover:shadow-lg transform transition-transform hover:-translate-y-2">
       <div className="relative">
         {/* Product Image */}
         <div className="h-[120px] md:h-[140px] xl:h-[160px] bg-gray-300">
-          {product_image && (
+          {product_image && !productImageError ? (
             <img
               src={product_image}
               alt={product_name}
               className="w-full h-full object-cover"
+              onError={() => setProductImageError(true)}
             />
+          ) : (
+            <div className="flex items-center justify-center h-[120px] md:h-[140px] xl:h-[160px] bg-gray-300">
+              <PiImageBroken height={120} width={120} />
+            </div>
           )}
         </div>
       </div>
       <div className="flex flex-col flex-grow p-2 space-y-2">
         <div className="w-full flex flex-row content-between justify-between space-x-4">
           {/* Brand Logo */}
-          {brand_image && (
+          {brand_image && !brandImageError ? (
             <img
               src={brand_image}
               alt={brand_name}
               className="h-4 rounded-sm"
+              onError={() => setBrandImageError(true)}
             />
+          ) : (
+            <PiImageBroken height={44} width={44} />
           )}
           {/* Brand Name*/}
           {brand_name && (
